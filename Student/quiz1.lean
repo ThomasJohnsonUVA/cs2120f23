1b8757b2703951e5d049f897fee1d0b04dee2255
Thomas Johnson <qrp4wr@virginia.edu>
2023-09-03 00:56:30 +0000

    idk why broke

---
+++ Student/quiz1.lean
@@ -0,0 +1,31 @@
+/-!
+# CS2120 F23 Quiz #1
+
+Welcome to quiz #1. The main purpose of this quiz 
+is to make sure you're set up with the techology you
+need for this class. 
+-/
+
+/- Question #1:
+
+What is the type of (the identifier) Bool in Lean?
+
+Replace the blank with your answer here: Type
+
+Remember you can use Lean to #check your answer!
+-/
+
+
+
+/-! Question #2:
+
+Is class moving too fast, too slow, or about right for you?
+
+Write your answer here: I think About Right
+
+If you said "too fast," please explain briefly what ideas
+you're most struggling with, here: 
+
+****** When I did git pull upstream main it gave me a merge error and I told it to merge with conflicts because
+I didn't know what else to do and it just cleared this quiz1.lean. So I recopied it over and refilled what I put before.***
+-/
